bce82461dcf34c12aeee33b8c6a6d1e2dc99cb15
tosamau <voyage33780@gmail.com>
2023-02-25 22:05:47 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,4 +1,17 @@
-//const e = require("turbolinks");
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;    //XHR.response.postと記述することで、レスポンスの中から投稿されたメモの情報を抽出し、変数itemに格納
+  const html = `
+  <div class="post">
+    <div class="post-date">
+      投稿日時:${item.created_at}
+    </div>
+    <div class="post-content">
+      ${item.content}
+      </div>
+    </div>`;
+    return html;  //変数htmlのこと
+};
+
 
 function post (){
   const submit = document.getElementById("submit");
@@ -10,7 +23,16 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
-    //console.log("イベント発火")
+    XHR.onload = () => {
+      if (XHR.status !=200) {         //HTTPステータスコードが格納されている
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);  //XHR.statusTextには、ステータスコードに応じたメッセージが格納される
+        return null;      //return null;によってJavaScriptの処理から抜け出すことができる
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");    //リセットの対象となるフォームの要素contentを取得して、変数formTextに格納     
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";       //フォームの中身をリセット
+    };
   });
 };
 
